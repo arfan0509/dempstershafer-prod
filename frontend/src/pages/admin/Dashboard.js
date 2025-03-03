3aaf2f95f10b31b60c3e7d0e7eb7bb23f0f35031
arfan0509 <arfanraja89@gmail.com>
2025-03-03 23:13:40 +0700

    again

--- frontend/src/pages/admin/Dashboard.js
+++ frontend/src/pages/admin/Dashboard.js
@@ -1,4 +1,6 @@
 import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect } from "react";
 import axiosInstance from "../../api/axiosInstance";
 import { XAxis, YAxis, Tooltip, PieChart, Pie, Cell, BarChart, Bar, ResponsiveContainer, } from "recharts";
